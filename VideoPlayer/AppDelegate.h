ecab45a4bd6e1dac1768eacb6692b18fd9ae3a48
Leonard Schuetz <leni.schuetz13@gmail.com>
2013-12-30 05:10:27 +0100

    Added features / Debugging
    Added features:
    - Save videoIDs
    - Autoplay when clicking on a saved videoID
    Added Credits page

--- VideoPlayer/AppDelegate.h
+++ VideoPlayer/AppDelegate.h
@@ -7,9 +7,24 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <VideoToolbox/VideoToolbox.h>
+#import <WebKit/WebKit.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+    IBOutlet WebView *webview;
+    IBOutlet NSPopUpButton *popupbutton;
+    
+    NSMutableArray *videoIDs;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain) IBOutlet WebView *webview;
+@property (nonatomic, retain) IBOutlet NSButton *load;
+@property (nonatomic, retain) IBOutlet NSTextField *textfield;
+@property (nonatomic, retain) IBOutlet NSPopUpButton *popupbutton;
+
+-(IBAction)loadURL:(id)sender;
+-(IBAction)update:(id)sender;
 
 @end
--- VideoPlayer/AppDelegate.m
+++ VideoPlayer/AppDelegate.m
@@ -1,18 +1,66 @@
-//
-//  AppDelegate.m
-//  VideoPlayer
-//
-//  Created by Leonard Schuetz on 28.12.13.
-//  Copyright (c) 2013 Leonard Schuetz. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+@synthesize webview;
+@synthesize load;
+@synthesize textfield;
+@synthesize popupbutton;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+//Initialized UserDefaults Values
+//      videoIDs            - NSArray
+//      notFirstRun         - bool
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSLog(@"applicationDidFinishLaunching");
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if (![defaults boolForKey:@"notFirstRun"]) {
+        NSLog(@"First run!");
+        [defaults setBool:YES forKey:@"notFirstRun"];
+        
+        //Initialize the array
+        videoIDs = [[NSMutableArray alloc] initWithObjects:@"Videos", @"E2VCwBzGdPM", nil];
+        [defaults setObject:videoIDs forKey:@"videoIDs"];
+        [popupbutton removeAllItems];
+        [popupbutton addItemsWithTitles:videoIDs];
+    } else {
+        NSLog(@"Not first run!");
+        [defaults setBool:YES forKey:@"notFirstRun"];
+        //Initialize the array
+        videoIDs = [[NSMutableArray alloc] initWithArray:[defaults objectForKey:@"videoIDs"]];
+        [defaults setObject:videoIDs forKey:@"videoIDs"];
+        [popupbutton removeAllItems];
+        [popupbutton addItemsWithTitles:videoIDs];
+    }
+}
+
+-(IBAction)loadURL:(id)sender {
+    NSLog(@"Load button got pressed");
+    [self loadURL_v];
+}
+
+- (IBAction)update:(id)sender {
+    NSLog(@"NSPopUpButton update");
+    //Get's called when the popupbutton get's changed
+    [textfield setStringValue:[popupbutton titleOfSelectedItem]];
+    [self loadURL_v];
+}
+
+-(void)loadURL_v {
+    NSLog(@"URL loads");
+    //Load the video
+    NSString *VIDEO_ID = textfield.stringValue;
+    NSString *VIDEO_BaseURL = @"http://www.youtube.com/embed/";
+    NSString *VIDEO_URL = [NSString stringWithFormat:@"%@%@", VIDEO_BaseURL, VIDEO_ID];
+    NSURL *VIDEO_URLtl = [NSURL URLWithString:VIDEO_URL];
+    [[webview mainFrame] loadRequest:[NSURLRequest requestWithURL:VIDEO_URLtl]];
+    
+    //Add the textfieldvalue to the popupbutton
+    [popupbutton addItemWithTitle:[textfield stringValue]];
+    
+    //Save videoIDs
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    videoIDs = [[NSMutableArray alloc] initWithArray:[popupbutton itemTitles]];
+    [defaults setObject:videoIDs forKey:@"videoIDs"];
 }
 
 @end
